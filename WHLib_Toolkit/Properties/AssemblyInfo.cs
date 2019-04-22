70d006974972930ab593908cc436289280f9bc14
Wu Hao <hwu.c@blizzard.com>
2019-04-22 16:00:53 +0800

    Add: Nuget

--- WHLib_Toolkit/Properties/AssemblyInfo.cs
+++ WHLib_Toolkit/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("WHLib_Toolkit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Elysium")]
 [assembly: AssemblyProduct("WHLib_Toolkit")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
